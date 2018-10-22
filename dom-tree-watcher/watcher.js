a01ec658692e1e6402b3101fd67a9d7f150dbc92
Maxim Fainberg <maksym.fainberg@mobenga.com>
2018-10-22 08:41:20 +0200

    DOM tree watcher - it works

--- dom-tree-watcher/watcher.js
+++ dom-tree-watcher/watcher.js
@@ -1,19 +1,34 @@
-function watch(selector) {
+function watch(selector, options = {skipIdentical: true}) {
     const nodes = [...document.querySelectorAll(selector)];
     if (!nodes.length) {
         console.log('No elements found in DOM');
         return;
     }
     const config = {
+        characterData: true,
 	    attributes: true,
 	    childList: true,
 	    subtree: true
+        // attributeOldValue: true,
+        // characterDataOldValue: true,
+        // attributeFilter: true
     };
     const items = nodes.map(node => ({history: [{time: Date.now(), state: node.outerText}]}));
     const callback = (item, mutations, observer) => {
-        const historyItem = {time: Date.now()};
-        mutations.forEach(mutationRecord => historyItem.state = mutationRecord.target.outerText);
-        item.history.push(historyItem);
+        const time = Date.now();
+        mutations.forEach(mutationRecord => {
+            const property = mutationRecord.type === 'characterData' ? 'data' : 'outerText'; 
+            const state = mutationRecord.target[property];
+            const prevState = item.history[item.history.length - 1].state;
+            if (!options.skipIdentical || state !== prevState) {
+                historyItem = {
+                    type: mutationRecord.type,
+                    state: state,
+                    time: time
+                };
+                item.history.push(historyItem);
+            }
+        });    
     };
     const observers = items.map(item => new MutationObserver(callback.bind(null, item))); 
     nodes.forEach((node, index) => observers[index].observe(node, config));
